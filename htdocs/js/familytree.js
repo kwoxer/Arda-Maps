11214d90a3cfccc45a9fc0f9e556fe0dc2a5b928
cool-Blue <cool.blue@y7mail.com>
2015-08-28 00:53:49 +1000

    info.css - added inforelations ul style
    info.js
      Theme
      - add an event to signal changes in width of the info window
         this is to allow family tree to adjust the center of it's graph
    - wrapped in infoState object
    - added an events object (so far only includes widthChangfe
      function toggle
      - added to encapsulate #infobutton.click behaviour
      - also emits the widthChange event
    - moved #infobutton.click code into toggle
      function width
      - added
      return width, show and events. events also bound directly to the return object
    
    familytree
      Theme
      - When a search list entry is clicked, highlight and center the node
      - Make this the CONTROLLER, eliminate references between orientdb and familytree-d3 all directed through here
      - Listen for graph_init event from familytree-d3 and init width change listener for infoState
    
    orientdb.js
     Theme
     - Make this the MODEL and all interaction via the CONTROLLER, no reference needed for familytree-d3
     - Use d3.map to manage link tables on the nodes instead of link counting
       (this is to support future communications between nodes and links)
     - Allow for asynchronous fetches for single nodes, fetches are staged and then later retrieved
     - Simplified interface with info.js using infoState object
     - Add asynchronous get for updating relationships in the info panel
       (TODO add relationsPanel property to infoState)
     - Fixed a bug in the locations update for the info panel
       (TODO add locationsPanel property to infoState)
     - Added generalise list management function
       (TODO use this for all lists and move management code to infoState)
    
    familytree-d3.js
      Theme
      - Make this a VIEW object and send all signalling via the CONTROLLER
      - Add behaviour to center a selected node and highlight it
      - Encapsulate force state and behaviour in fdg
      - Eliminate hard references to the parent object (familytree)
      - Removed all unnecessary properties exposed on familytree
      - Moved all zoom and drag management into zoomableSVG
      - Expose internal state and behaviour by direct binding onto fdg and family tree (use d3.rebind)
      - Add start, end and progress events for force, emitted from tick
      - Eliminate switch statements, use hash tables instead
      - Eliminate the need to access familytree events via .events, bind directly onto famillytree
    
    index.html
    - set version to 3.a
    - moved info.js above orientdb.js

--- htdocs/js/familytree.js
+++ htdocs/js/familytree.js
@@ -1,82 +1,117 @@
 var familytreeController = (function() {
-    var suggestions = "#familytreesearchsuggestions";
-    var suggestionsFamily = "#familytreesearchsuggestionsFamilycreatures";
-    var showAll;
+  var suggestions = "#familytreesearchsuggestions";
+  var suggestionsFamily = "#familytreesearchsuggestionsFamilycreatures";
+  var showAll;
+  var zoomToTransition = 1000;
 
-    $("#familytreecontentclose a").click(function() {
-        $("#familytree").hide();
-        $("#familytreecontent").children().hide();
-        $("#illustrator").css("z-index", "19");
-    });
+  $("#familytreecontentclose a").click(function() {
+    /** TODO?
+     * Add shut-down code for familytree object?
+     */
+    $("#familytree").hide();
+    $("#familytreecontent").children().hide();
+    $("#illustrator").css("z-index", "19");
+    infoState.on("widthChange", null);
+  });
 
-    $("#familytreeShowallbutton").click(function() {
-        showAll = true;
-        orientdb.getFamilytreeAll2(familytree.initializeGraph);
-    });
+  $("#familytreeShowallbutton").click(function() {
+    showAll = true;
+    orientdb.getFamilytreeAll2(familytree.initializeGraph);
+  });
 
-    $("#familytreeHideallbutton").click(function() {
-        orientdb.clearAll(familytree.initializeGraph);
-    });
+  $("#familytreeHideallbutton").click(function() {
+    orientdb.clearAll(familytree.initializeGraph);
+  });
 
-    d3.select('#familytreeUnfixallbutton').on('click', function() {
-        d3.selectAll('#familytreecontentsvg .node')
-            .each(function(d) {
-                d.fixed = false;
-            })
-            .classed("fixed", false)
-    });
+  d3.select('#familytreeUnfixallbutton').on('click', function() {
+    d3.selectAll('#familytreecontentsvg .node')
+      .each(function(d) {
+        d.fixed = false;
+      })
+      .classed("fixed", false)
+  });
 
-    $("#familytreesearch").on('input', function() {
-        if($("#familytreesearch").val() == "") {
-            $(suggestions).hide();
-        } else {
-            $(suggestions).show();
-            orientdb.search4Creature("#familytreesearch", suggestionsFamily);
-        }
-    });
-
-    $("ul" + suggestionsFamily).on('click', 'li', function() {
-        if(showAll) orientdb.clearAll();
-        orientdb.getFamilytreeSingle2(this.id, familytree.initializeGraph);
-        showAll = false;
-        $(this).addClass("active");
-    });
-
-    familytree.events.on("node_dblclick", function(d) {
-        orientdb.getFamilytreeSingle2(d.ID + '|' + d.class, familytree.initializeGraph);
-    });
-
-    familytree.events.on("node_click", function(d) {
-        orientdb.getInfo4CreatureByRID(d.ID);
-    });
-
-    familytree.events.on("node_contextmenu", function(clickedNode) {
-        orientdb.deleteCreatureByRID(clickedNode, familytree.initializeGraph);
-    });
-
-    $("ul" + suggestionsFamily).on('mouseenter mouseleave', 'li', function() {
-        $(this).toggleClass("highlight");
-    });
-
-    $("#familytreesearch").attr('autocomplete', 'off');
-
-    $("#familytreecontentclose").mouseenter(function() {
-        if(ardamap.getCurrentAge() == "") {
-            $("#familytreeinner a").attr("href", "/");
-        }
-        if(ardamap.getCurrentAge() == "first") {
-            $("#familytreeinner a").attr("href", "/ages/first/");
-        }
-        if(ardamap.getCurrentAge() == "second") {
-            $("#familytreeinner a").attr("href", "/ages/second/");
-        }
-        if(ardamap.getCurrentAge() == "third") {
-            $("#familytreeinner a").attr("href", "/ages/third/");
-        }
-    });
-
-    function getRelationships(id, mode) {
-        if(showAll) familytree.cleanPresentation(mode);
-        orientdb.getFamilytreeSingle2(id, familytree.updateGraph);
+  $("#familytreesearch").on('input', function() {
+    if($("#familytreesearch").val() == "") {
+      $(suggestions).hide();
+    } else {
+      $(suggestions).show();
+      orientdb.search4Creature("#familytreesearch", suggestionsFamily);
     }
+  });
+
+  $("ul" + suggestionsFamily).on('click', 'li', function() {
+    //if(showAll)  orientdb.clearAll();
+    var id = this.id.split("|")[0];
+    orientdb.stageFamilytreeSingle(this.id, function() {
+      var n = this.mergeSingle().dataSet(familytree.initializeGraph).nodes[id],
+        stages = "force_stage", stopEvent = "force_stop",
+        eventID = Date.now();
+      if(n) {
+        orientdb.getInfo4CreatureByRID(id);
+        familytree.focusNode(n).highlight();
+        familytree.on([stages, eventID].join("."), function(){
+          familytree.zoomTo(n);
+        });
+        familytree.on([stopEvent, eventID].join("."), function() {
+          familytree.focusNode(n).highlight().delay(2000).blur();
+          familytree.on("." + eventID, null);
+          console.log(familytree.on([stopEvent, eventID].join(".")))
+        })
+      }
+    });
+    //showAll = false;
+    $(this).addClass("active");
+  });
+
+  familytree.on("node_dblclick", function(d) {
+    //familytree.zoomTo(d);
+    // try to get the relationship tree for the selected node and asynchronously
+    // merge it into the current tree if successful
+    orientdb.stageFamilytreeSingle(d.ID + '|' + d.class, function() {
+      this.mergeSingle().dataSet(familytree.initializeGraph)
+    });
+  });
+
+  familytree.on("node_click", function(d) {
+    orientdb.getInfo4CreatureByRID(d.ID);
+  });
+
+  familytree.on("node_contextmenu", function(clickedNode) {
+    orientdb.deleteCreatureByRID(clickedNode, familytree.initializeGraph);
+  });
+
+  $("ul" + suggestionsFamily).on('mouseenter mouseleave', 'li', function() {
+    $(this).toggleClass("highlight");
+  });
+
+  $("#familytreesearch").attr('autocomplete', 'off');
+
+  $("#familytreecontentclose").mouseenter(function() {
+    if(ardamap.getCurrentAge() == "") {
+      $("#familytreeinner a").attr("href", "/");
+    }
+    if(ardamap.getCurrentAge() == "first") {
+      $("#familytreeinner a").attr("href", "/ages/first/");
+    }
+    if(ardamap.getCurrentAge() == "second") {
+      $("#familytreeinner a").attr("href", "/ages/second/");
+    }
+    if(ardamap.getCurrentAge() == "third") {
+      $("#familytreeinner a").attr("href", "/ages/third/");
+    }
+  });
+  function getRelationships(id, mode) {
+    if(showAll) familytree.cleanPresentation(mode);
+    orientdb.getFamilytreeSingle2(id, familytree.updateGraph);
+  }
+
+  familytree.on("graph_init", function(){
+    infoState.on("widthChange", function(deltaW){
+      if(familytree.data()) {
+        familytree.zoomTo();
+        familytree.focusNode().highlight().delay(2000).blur();
+      }
+    })
+  })
 })();
